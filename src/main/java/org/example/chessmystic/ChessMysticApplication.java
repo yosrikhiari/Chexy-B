73681e6c0d4ee4569c06189a9f2e57095070122b
khiariyosri <khiariyosri@esprit.tn>
2025-06-21 10:52:10 +0100

    idk 1

--- src/main/java/org/example/chessmystic/ChessMysticApplication.java
+++ src/main/java/org/example/chessmystic/ChessMysticApplication.java
@@ -2,11 +2,14 @@ package org.example.chessmystic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ChessMysticApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(ChessMysticApplication.class, args);
     }
 
